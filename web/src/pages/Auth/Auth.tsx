6327f391c048e37e42c795997d2df8a44b1bbb2f
Elijah <elijahreck@gmail.com>
2026-06-06 19:31:52 +0000

    Started working on auth page

--- web/src/pages/Auth/Auth.tsx
+++ web/src/pages/Auth/Auth.tsx
@@ -1,10 +1,21 @@
+import { createSignal } from "solid-js"
 import Card from "../../components/Card"
 import styles from "./Auth.module.css"
 
 export default () => {
+    const [email, setEmail] = createSignal<string>("");
+    const [password, setPassword] = createSignal<string>("");
+    
     return (
         <div id={styles.authpage}>
-            <Card styles="width: 80%; height: 400px;">Auth</Card>
+            <Card styles="width: 80%; height: 400px;">
+                <form>
+                    <label>Email</label>
+                    <input id="auth-email" type="email" value={email()} onChange={(e) => setEmail(e.target.value)}/>
+                    <label>Password</label>
+                    <input id="auth-password" type="password" value={password()} onChange={(e) => setPassword(e.target.value)}/>/>
+                </form>
+            </Card>
         </div>
     )
 }
