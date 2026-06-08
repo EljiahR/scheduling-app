25e9e5748b6796c445e33dd4ff10b6bdc3928f3a
Elijah <elijahreck@gmail.com>
2026-06-08 13:10:40 +0000

    Added a password visibility toggle to auth form

--- web/src/pages/Auth/Auth.tsx
+++ web/src/pages/Auth/Auth.tsx
@@ -5,28 +5,35 @@ import styles from "./Auth.module.css"
 export default () => {
     const [email, setEmail] = createSignal<string>("");
     const [password, setPassword] = createSignal<string>("");
+    const [isPasswordVisible, setIsPasswordVisible] = createSignal<boolean>(false);
 
     const handleFormSubmit = (e: SubmitEvent) => {
         e.preventDefault();
+    };
 
-        
+    const handleViewPasswordToggle = () => {
+        setIsPasswordVisible((isPasswordVisible) => !isPasswordVisible);
     };
     
     return (
-        <div id={styles.authpage}>
+        <div id={styles.authPage}>
             <Card styles="width: 80%; max-width: 500px; height: 400px; display: flex; justify-content: center; align-items: center;">
-                <form id={styles.authform} onSubmit={(e) => handleFormSubmit(e)}>
-                    <div class={styles.floatlabel}>
+                <form id={styles.authForm} onSubmit={(e) => handleFormSubmit(e)}>
+                    <div class={styles.floatLabel}>
                         <input id="auth-email" type="email" value={email()} onChange={(e) => setEmail(e.target.value)} placeholder="" required />
                         <label for="auth-email">Email</label>
                     </div>
-                    <div class={styles.floatlabel}>
-                        <input id="auth-password" type="password" value={password()} onChange={(e) => setPassword(e.target.value)} placeholder="" required />
+                    <div class={styles.floatLabel}>
+                        <input id="auth-password" type={isPasswordVisible() ? "text" : "password"} value={password()} onChange={(e) => setPassword(e.target.value)} placeholder="" required />
                         <label for="auth-password">Password</label>
+                    </div>
+                    <div id={styles.visiblePassword}>
+                        <input id="visible-password-toggle" type="checkbox" checked={isPasswordVisible()} onClick={handleViewPasswordToggle} />
+                        <label for="visible-password-toggle">Show Password?</label>
                     </div>
                     <button type="submit">Sign In</button>
                 </form>
             </Card>
         </div>
     )
-}
+};
