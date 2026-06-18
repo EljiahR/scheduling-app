6c4b96832b8d204d066de33ee2c9100468fbe83e
Elijah <elijahreck@gmail.com>
2026-06-18 00:08:00 +0000

    Minor updates to Navbar

--- web/src/components/NavBar/NavBar.tsx
+++ web/src/components/NavBar/NavBar.tsx
@@ -1,13 +1,14 @@
 import { A } from "@solidjs/router";
 import "../../global.css";
 import styles from "./NavBar.module.css";
+import HomeSVG from "./home.svg";
+import HamburgerSVG from "./hamburger-menu.svg";
 
 export default () => {
     return (
         <nav id={styles.navbar} class="theme">
-            <A href="/" class="navlink">Home</A>
-            <A href="/protected" class="navlink">Protected</A>
-            <A href="/auth/checkAuth" class="navlink">Auth</A>
+            <button><HamburgerSVG height={"30px"} width={"30px"} class="white-svg" /></button>
+            <A href="/" class="navlink"><HomeSVG height={"30px"} width={"30px"} class="white-svg" /></A>
         </nav>
     )
 }
